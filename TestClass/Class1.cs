7b5ba7f4f4c28d2d55d549c27b3c38d44949cbc1
suren <suren@SURENDERREDDY>
2023-02-23 05:51:00 +0530

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnitDemonstration;
+using NUnit.Framework;
+
+namespace TestClass
+{
+    public class TriangleTest
+    {
+        [TestFixture]
+        public class TriangleTest1
+        {
+            [Test]
+
+            public void ValidTriangle_Input_60and60and60_OutputvalidTriangle()
+            {
+
+                int firstAngle = 60;
+                int secondAngle = 400;
+                int thirdAngle = 60;
+
+                String expected = "The triangle is valid.";
+
+                String Actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                Assert.AreEqual(expected, Actual);
+            }
+        }
+    }
+}
